4422326340273c4cef2de7be1cb592c1b8664289
krunal <krunalgajbhiye5@gmail.com>
2022-12-20 13:20:01 +0530

    krunal

---
+++ src/main/java/com/sysco/loadbancer/targetgroup/RequestHandler.java
@@ -0,0 +1,7 @@
+package com.sysco.loadbancer.targetgroup;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class RequestHandler extends SpringBootRequestHandler<String,Object>{
+
+}
